7ee021eb19d17b4cf56c63be2cf6d33a5fb7d10a
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-03-24 09:18:32 -0300

    Tela do education

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Wallet from './Wallet';
+import Education from './Education';
 
 class App extends Component {
     render() {
@@ -13,6 +14,7 @@ class App extends Component {
                         <Route path="/" element={<Home/>}/>
                         <Route path="/login" element={<Login/>}/>
                         <Route path="/wallet" element={<Wallet/>}/>
+                        <Route path="/education" element={<Education/>}/>
                     </Routes>
                 </div>
             </div>
---
+++ client/src/components/Education.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import logo from './imgs/logopreto.png';
+import img1 from './imgs/card.png'; 
+import img2 from './imgs/card2.png';
+import img3 from './imgs/card3.png'; 
+import img4 from './imgs/card4.png'; 
+import img5 from './imgs/carteira.png';
+import img6 from './imgs/hackathons.png'; 
+import img7 from './imgs/ranking.png'; 
+import { Link } from 'react-router-dom';
+
+const Education = () => {
+    return (
+        <div className="education">
+            <br />
+            <div className="logozinha">
+                <Link to="/">
+                    <img alt="logo" src={logo} />
+                </Link>
+            </div>
+            <br />
+            <div className="menu">
+                <Link className="linkinzin2" to="/wallet">
+                    <p>Home</p>
+                </Link>
+                <Link className="linkinzin2" to="/history">
+                    <p>History</p>
+                </Link>
+                <Link className="linkin2" to="/education">
+                    <p className="ativado">Education</p>
+                </Link>
+            </div>
+            <br />
+            <div className="gallery">
+                <div className="row">
+                    <img className="square" src={img1} alt="Imagem 1" />
+                    <img className="rectangle" src={img2} alt="Imagem 2" />
+                </div>
+                <div className="row">
+                    <img className="rectangle" src={img3} alt="Imagem 3" />
+                    <img className="square" src={img4} alt="Imagem 4" />
+                </div>
+                <div className="row">
+                    <img className="square2" src={img5} alt="Imagem 5" />
+                    <img className="square2" src={img6} alt="Imagem 6" />
+                    <img className="square2" src={img7} alt="Imagem 7" />
+                </div>
+            </div>
+            <br/>
+            <br/>
+        </div>
+    );
+};
+
+export default Education;
