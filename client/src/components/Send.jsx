036a0a2ae13146a37284b6f43de7266154efe7f6
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-03-29 16:19:49 -0300

    Finalização da tela de Send

--- client/src/components/Send.jsx
+++ client/src/components/Send.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef } from "react";
 import logo from './imgs/layer3.png';
 import { Link } from 'react-router-dom';
+import xion from './imgs/xion_token.png';
+import eth from './imgs/eth_token.png';
+import button_two from './imgs/Sent.png';
 
 const Send = () => {
     return (
@@ -13,8 +16,36 @@ const Send = () => {
             </div>
             <br />
             <div className="carteira">
+                <p className="primeiro">From</p>
+                <div class="left">
+                    <p className="pequena2">xion1sjv8p79cwm08m8nm8e6yntuvjxj53hd7e4l6y3</p>
+                    <img className="token" alt="xion" src={xion} />
+                </div>
+                <br />
+                <p className="primeiro">To</p>
+                <div class="left">
+                    <input className="pequena4"></input>
+                    <img className="token" alt="eth" src={eth} />
+                </div>
+                <br />
+                <p className="primeiro">Value</p>
+                <div class="left">
+                    <input type="number" className="pequena4"></input>
+                    <img className="token" alt="eth" src={xion} />
+                    <p className="pequena2">ETH 0.00</p>
+                </div>
+                <br />
+                <center>
+                    <hr />
+                    <br/>
+                    <button>
+                        <img src={button_two} alt="Share" />
+                        Send
+                    </button>
+                </center>
+                <div></div>
             </div>
-            <br/><br/>
+            <br /><br />
         </div>
     );
 };
